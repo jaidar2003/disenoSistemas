a3d9a97f9c7081df7f742d1a8cecb65ec6156287
Juan Manuel Aidar <127959590+jaidar2003@users.noreply.github.com>
2024-11-08 23:23:44 -0300

    Delete models directory

--- models/dna_model.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String, Boolean
-from database.db_connection import Base
-
-class Dna(Base):
-    __tablename__ = 'dna_records'
-
-    id = Column(Integer, primary_key=True)
-    dna = Column(String, unique=True, nullable=False)
-    is_mutant = Column(Boolean, nullable=False)
